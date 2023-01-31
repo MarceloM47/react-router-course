410152dcf9a76ab746f6c43df610e76be2568fb8
Marcelo Morinigo <78925750+MarceloM47@users.noreply.github.com>
2023-01-31 01:12:12 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import UsersPage from './pages/UsersPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/about" element={<AboutPage/>} />
+      <Route path="/users" element={<UsersPage/>} />
+      <Route path="*" element={<NotFoundPage/>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
